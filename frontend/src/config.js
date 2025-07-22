bdf1639932cded4d5ea8d048a8bddbd81dcd9e92
Gorav Gumber <163832113+Gauravg2630@users.noreply.github.com>
2025-07-22 10:18:48 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
-const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
-const BASE_URL = "https://newsapi.org/v2";
+const BASE_URL = "https://news-api-backend-yb9z.onrender.com/api";
 
-export { API_KEY, BASE_URL };
+export { BASE_URL };
